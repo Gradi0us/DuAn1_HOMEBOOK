5027f4e2d8bb0768413ecf328204b2582f2a97a6
L. J Nguyen <loinguyengamer2504@gmail.com>
2022-11-11 07:22:41 +0700

    fix bug

---
+++ HOMEBOOK/app/src/main/java/com/example/home_book/Language.java
@@ -0,0 +1,20 @@
+package com.example.home_book;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Language{
+
+    public void callView(){
+
+    }
+
+    public void vietnameseLanguage(){
+
+
+    }
+
+    public void englishLanguage(){
+
+    }
+}
