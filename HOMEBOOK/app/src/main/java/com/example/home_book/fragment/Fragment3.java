9b0ec27037f9bcb4fda715888bbe8eae66927b55
duongndtph25724 <duongndtph25724@fpt.edu.vn>
2022-11-14 13:30:02 +0700

    duongndtph25724

--- HOMEBOOK/app/src/main/java/com/example/home_book/fragment/Fragment3.java
+++ HOMEBOOK/app/src/main/java/com/example/home_book/fragment/Fragment3.java
@@ -1,5 +1,6 @@
 package com.example.home_book.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.example.home_book.BottomNavActivity;
+import com.example.home_book.ChangePassActivity;
+import com.example.home_book.LienHeActivity;
 import com.example.home_book.R;
+import com.example.home_book.TaiKhoan;
+import com.example.home_book.ThongTin;
+import com.example.home_book.adapter.ListMenuAdapter;
+import com.example.home_book.model.ListModelMenu;
+
+import java.util.ArrayList;
 
 public class Fragment3 extends Fragment {
 
+    ListView lv;
+
+
     public Fragment3() {
-        // Required empty public constructor
     }
 
 
@@ -22,6 +36,43 @@ public class Fragment3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_3, container, false);
+
+        ListView listView = v.findViewById(R.id.lvmenu);
+
+        ArrayList<ListModelMenu> list = new ArrayList<>();
+
+        //add vao listview
+        list.add(new ListModelMenu(R.drawable.setting_item, "Quản lý tài khoản"));
+        list.add(new ListModelMenu(R.drawable.changepass_item, "Đổi mật khẩu"));
+        list.add(new ListModelMenu(R.drawable.contact, "Liên hệ"));
+        list.add(new ListModelMenu(R.drawable.in4, "Thông tin Ứng dụng"));
+        list.add(new ListModelMenu(R.drawable.exit, "Thoát ứng dụng"));
+
+        ListMenuAdapter adapter = new ListMenuAdapter(getContext(), R.layout.item_menu, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+                    startActivity(new Intent(getContext(), TaiKhoan.class));
+                }
+                if(i==1){
+                    startActivity(new Intent(getContext(), ChangePassActivity.class));
+                }
+                if(i==2){
+                    startActivity(new Intent(getContext(), LienHeActivity.class));
+                }
+                if(i==3){
+                    startActivity(new Intent(getContext(), ThongTin.class));
+                }
+                if(i==4){
+                    System.exit(0);
+                }
+
+
+            }
+        });
 
 
         return v;
