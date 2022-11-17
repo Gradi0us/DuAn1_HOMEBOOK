b7c95f3d920747cab345887ef915494a9a3d1da7
Gradi0us <hieunguyen20032302@gmail.com>
2022-11-17 23:32:46 +0700

    Hieund Model

--- HOMEBOOK/app/src/main/java/com/example/home_book/fragment/LoginFragment.java
+++ HOMEBOOK/app/src/main/java/com/example/home_book/fragment/LoginFragment.java
@@ -1,5 +1,9 @@
 package com.example.home_book.fragment;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +12,25 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.home_book.DAO.DAO;
 import com.example.home_book.R;
+import com.example.home_book.activity.LoginActivity;
+import com.example.home_book.activity.MainActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class LoginFragment extends Fragment {
 
-
+    TextInputEditText emailIn,passIn;
+    CheckBox remember;
+    TextView forget;
+    Button signIn;
+    DAO dao;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +48,63 @@ public class LoginFragment extends Fragment {
                    .replace(R.id.frame,new Fragment3())
                    .commit();
        });
+        emailIn = view.findViewById(R.id.emailIn);
+        passIn = view.findViewById(R.id.passIn);
+        signIn = view.findViewById(R.id.signIn);
+        remember = view.findViewById(R.id.rememberBox);
+        forget = view.findViewById(R.id.quenMatKhau);
+        dao = new DAO(getContext());
+
+        String eI = emailIn.getText().toString();
+        String pI = passIn.getText().toString();
+
+        SharedPreferences sP = getActivity().getSharedPreferences("User_File",MODE_PRIVATE);
+        String email = sP.getString("Email","");
+        String pass = sP.getString("Password","");
+        Boolean rem = sP.getBoolean("Remember",false);
+
+        emailIn.setText(email);
+        passIn.setText(pass);
+        remember.setChecked(rem);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sP.edit();
+                Boolean check = true;
+                if(eI.trim().length() <= 0){
+                    check = false;
+                }
+                if(pI.trim().length() <=0){
+                    check = false;
+                }
+                if(check){
+//                    if(dao.checkLogin(eI,pI)){
+//                        rememberUser(eI,pI,remember.isChecked());
+//                        editor.putString("Email",eI);
+//                        editor.commit();
+//                        Toast.makeText(getActivity(), "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+//                        startActivity(new Intent(getActivity(), MainActivity.class));
+//                    }else{
+//                        Toast.makeText(getActivity(), "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+//                    }
+                }
+            }
+        });
         return view;
     }
-}
+
+    public void rememberUser(String e,String p, boolean s){
+        SharedPreferences pref = getActivity().getSharedPreferences("User_File",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        if(s){
+            editor.putString("Email",e);
+            editor.putString("Password",p);
+            editor.putBoolean("Remember",s);
+        }else{
+            editor.clear();
+        }
+        editor.commit();
+    }
+
+    }
--- HOMEBOOK/app/src/main/java/com/example/home_book/fragment/RegisterFragment.java
+++ HOMEBOOK/app/src/main/java/com/example/home_book/fragment/RegisterFragment.java
@@ -1,5 +1,6 @@
 package com.example.home_book.fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,22 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
 import com.example.home_book.R;
+import com.example.home_book.activity.RegisterActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class RegisterFragment extends Fragment {
 
+    TextInputEditText birthUp;
+    int dD,mM,yY;
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
 
     @Override
@@ -33,6 +44,27 @@ public class RegisterFragment extends Fragment {
                     .replace(R.id.frame,new Fragment3())
                     .commit();
         });
+        DatePickerDialog.OnDateSetListener chonDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                yY = year; mM = month; dD = dayOfMonth;
+                GregorianCalendar gC = new GregorianCalendar(yY,mM,dD);
+                birthUp.setText(format.format(gC.getTime()));
+            }
+        };
+        birthUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                yY = calendar.get(Calendar.YEAR);
+                mM = calendar.get(Calendar.MONTH);
+                dD = calendar.get(Calendar.DATE);
+
+                DatePickerDialog d = new DatePickerDialog(getActivity(),0,chonDate,yY,mM,dD);
+                d.show();
+            }
+        });
+
         return view;
     }
 }
