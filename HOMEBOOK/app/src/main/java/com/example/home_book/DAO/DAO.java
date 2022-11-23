14bc04c4d316207b925908c33183c745d4d2ae2f
Gradi0us <hieunguyen20032302@gmail.com>
2022-11-23 20:17:13 +0700

    Update DAO.java

--- HOMEBOOK/app/src/main/java/com/example/home_book/DAO/DAO.java
+++ HOMEBOOK/app/src/main/java/com/example/home_book/DAO/DAO.java
@@ -6,12 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.home_book.database.AppSQL;
-import com.example.home_book.model.order;
-import com.example.home_book.model.roomImage;
 import com.example.home_book.model.rooms;
 
 import java.util.ArrayList;
-
 import com.example.home_book.model.user;
 
 import java.text.ParseException;
@@ -33,11 +30,11 @@ public class DAO {
     }
     /////////////////////////////////////////////////////////////////////
 
-    public boolean checkLogin(String email, String pass) {
+    public boolean checkLogin(String email,String pass) {
         String sql = "SELECT * FROM user_tb WHERE email=? and password=?";
         db = appSQL.getReadableDatabase();
-        Cursor cursor = db.rawQuery(sql, new String[]{email, pass});
-        if (cursor.getCount() != 0) {
+        Cursor cursor = db.rawQuery(sql,new String[]{email,pass});
+        if(cursor.getCount() != 0){
             return true;
         }
         return false;
@@ -70,69 +67,71 @@ public class DAO {
         return list;
     }
 
-    public user getUserId(String id) {
+    public user getUserId(String id){
         String sql = "select * from user_tb where id=?";
-        List<user> list = getUser(sql, id);
+        List<user> list = getUser(sql,id);
         return list.get(0);
     }
 
 
-    public long AddUser(user x) {
+    public long AddUser(user x){
         ContentValues value = new ContentValues();
-        value.put("avatar", x.getAvatar());
-        value.put("fullname", x.getFullname());
-        value.put("email", x.getEmail());
-        value.put("password", x.getPassword());
-        value.put("role", x.getRole());
-        value.put("birthday", format.format(x.getBirth_day()));
-        value.put("phonenumber", x.getPhone());
-        value.put("money", x.getMoney());
-        return db.insert("user_tb", null, value);
+        value.put("avatar",x.getAvatar());
+        value.put("fullname",x.getFullname());
+        value.put("email",x.getEmail());
+        value.put("password",x.getPassword());
+        value.put("role",x.getRole());
+        value.put("birthday",format.format(x.getBirth_day()));
+        value.put("phonenumber",x.getPhone());
+        value.put("money",x.getMoney());
+        return db.insert("user_tb",null,value);
     }
 
-    public long UpdateUser(user x) {
+    public long UpdateUser(user x){
         ContentValues value = new ContentValues();
-
-        value.put("avatar", x.getAvatar());
-        value.put("fullname", x.getFullname());
-        value.put("email", x.getEmail());
-        value.put("password", x.getPassword());
-        value.put("role", x.getRole());
-        value.put("birthday", format.format(x.getBirth_day()));
-        value.put("phonenumber", x.getPhone());
-        value.put("money", x.getMoney());
-        return db.update("user_tb", value, "id=?", new String[]{String.valueOf(x.getId())});
+        value.put("avatar",x.getAvatar());
+        value.put("fullname",x.getFullname());
+        value.put("email",x.getEmail());
+        value.put("password",x.getPassword());
+        value.put("role",x.getRole());
+        value.put("birthday",format.format(x.getBirth_day()));
+        value.put("phonenumber",x.getPhone());
+        value.put("money",x.getMoney());
+        return db.update("user_tb",value,"id=?",new String[]{String.valueOf(x.getId())});
     }
 
-//    public ArrayList<rooms> Search(String name, String trangthai) {
-//        ArrayList<rooms> list = new ArrayList<>();
-//        SQLiteDatabase sqLiteDatabase = appSQL.getReadableDatabase();
-//        Cursor cursor = sqLiteDatabase.rawQuery(" SELECT * FROM UnLock where name like '%" + name + "%' AND trangthai ='" + trangthai + "' ORDER BY name ASC", null);
-//        if (cursor.getCount() != 0) {
-//            cursor.moveToFirst();
-//            do {
-//                int id = cursor.getInt(cursor.getColumnIndex("id"));
-//                String link = cursor.getString(cursor.getColumnIndex("package"));
-//                String name1 = cursor.getString(cursor.getColumnIndex("name"));
-//                byte[] img = cursor.getBlob(cursor.getColumnIndex("img"));
-//                String trangthai1 = cursor.getString(cursor.getColumnIndex("trangthai"));
-//                String tinhtrang = cursor.getString(cursor.getColumnIndex("tinhtrang"));
-//                //Thêm dữ liệu vừa lấy ra từ cột vào đối tượng Laptop
-//                App app = new App();
-//                app.setId(id);
-//                app.setLinkpackage(link);
-//                app.setLabel(name1);
-//                app.setImg(img);
-////                app.setImg();
-//                app.setTrangthai(trangthai1);
-//                app.setTinhtrang(tinhtrang);
-//                //Thêm đối tượng vào danh sách
-//                list.add(app);
-//            } while (cursor.moveToNext());
-//        }
-//        return list;
+    public void DeletePhieuMuon(int ID){
+        db.delete("phieuMuon_tb","idphieu=?",new String[]{String.valueOf(ID)});
+    }
+
+//public ArrayList<rooms> Search(String name, String trangthai) {
+//    ArrayList<rooms> list = new ArrayList<>();
+//    SQLiteDatabase sqLiteDatabase = appSQL.getReadableDatabase();
+//    Cursor cursor = sqLiteDatabase.rawQuery(" SELECT * FROM UnLock where name like '%" + name + "%' AND trangthai ='" + trangthai + "' ORDER BY name ASC", null);
+//    if (cursor.getCount() != 0) {
+//        cursor.moveToFirst();
+//        do {
+//            int id = cursor.getInt(cursor.getColumnIndex("id"));
+//            String link = cursor.getString(cursor.getColumnIndex("package"));
+//            String name1 = cursor.getString(cursor.getColumnIndex("name"));
+//            byte[] img = cursor.getBlob(cursor.getColumnIndex("img"));
+//            String trangthai1 = cursor.getString(cursor.getColumnIndex("trangthai"));
+//            String tinhtrang = cursor.getString(cursor.getColumnIndex("tinhtrang"));
+//            //Thêm dữ liệu vừa lấy ra từ cột vào đối tượng Laptop
+//            App app = new App();
+//            app.setId(id);
+//            app.setLinkpackage(link);
+//            app.setLabel(name1);
+//            app.setImg(img);
+//                app.setImg();
+//            app.setTrangthai(trangthai1);
+//            app.setTinhtrang(tinhtrang);
+//            //Thêm đối tượng vào danh sách
+//            list.add(app);
+//        } while (cursor.moveToNext());
 //    }
-
+//    return list;
+//}
 
     public List<rooms> getRoom(String sql, String... args) {
         List<rooms> list = new ArrayList<>();
@@ -152,7 +151,7 @@ public class DAO {
             String service = c.getString(10);
             int cost = c.getInt(11);
             int status = c.getInt(12);
-            rooms x = new rooms(id, name, brand, category, max, beds, room, rate, note, size, service, cost, status);
+            rooms x = new rooms(id,name,brand,category,max,beds,room,rate,note,size,service,cost,status);
             list.add(x);
             c.moveToNext();
         }
@@ -160,126 +159,42 @@ public class DAO {
         return list;
     }
 
-    public long AddRoom(rooms x) {
+    public long AddRoom(rooms x){
         ContentValues value = new ContentValues();
-        value.put("fullname", x.getName());
-        value.put("brand_name", x.getBrand());
-        value.put("category_name", x.getCategory());
-        value.put("rate", x.getRate());
-        value.put("max_people", x.getMax_people());
-        value.put("beds", x.getBeds());
-        value.put("rooms", x.getRooms());
-        value.put("note", x.getNote());
-        value.put("size", x.getSize());
-        value.put("service", x.getService());
-        value.put("cost", x.getCost());
-        value.put("status", x.getStatus());
-        return db.insert("room_tb", null, value);
+        value.put("fullname",x.getName());
+        value.put("brand_name",x.getBrand());
+        value.put("category_name",x.getCategory());
+        value.put("rate",x.getRate());
+        value.put("max_people",x.getMax_people());
+        value.put("beds",x.getBeds());
+        value.put("rooms",x.getRooms());
+        value.put("note",x.getNote());
+        value.put("size",x.getSize());
+        value.put("service",x.getService());
+        value.put("cost",x.getCost());
+        value.put("status",x.getStatus());
+        return db.insert("room_tb",null,value);
     }
 
-    public long UpdateRoom(rooms x) {
+    public long UpdateRoom(rooms x){
         ContentValues value = new ContentValues();
-        value.put("fullname", x.getName());
-        value.put("brand_name", x.getBrand());
-        value.put("category_name", x.getCategory());
-        value.put("rate", x.getRate());
-        value.put("max_people", x.getMax_people());
-        value.put("beds", x.getBeds());
-        value.put("rooms", x.getRooms());
-        value.put("note", x.getNote());
-        value.put("size", x.getSize());
-        value.put("service", x.getService());
-        value.put("cost", x.getCost());
-        value.put("status", x.getStatus());
-        return db.update("room_tb", value, "id=?", new String[]{String.valueOf(x.getId())});
+        value.put("fullname",x.getName());
+        value.put("brand_name",x.getBrand());
+        value.put("category_name",x.getCategory());
+        value.put("rate",x.getRate());
+        value.put("max_people",x.getMax_people());
+        value.put("beds",x.getBeds());
+        value.put("rooms",x.getRooms());
+        value.put("note",x.getNote());
+        value.put("size",x.getSize());
+        value.put("service",x.getService());
+        value.put("cost",x.getCost());
+        value.put("status",x.getStatus());
+        return db.update("room_tb",value,"id=?",new String[]{String.valueOf(x.getId())});
     }
 
-    public void DeleteRoom(int ID) {
-        db.delete("room_tb", "id=?", new String[]{String.valueOf(ID)});
-    }
-
-    public List<order> getOrder(String sql, String... args) {
-        List<order> list = new ArrayList<>();
-        Cursor c = db.rawQuery(sql, args);
-        c.moveToFirst();
-        while (!c.isAfterLast()) {
-            Date ngayNhan = null , ngayTra = null;
-            int id = c.getInt(0);
-            int user_id = c.getInt(1);
-            int number = c.getInt(2);
-            try {
-                ngayNhan = format.parse(c.getString(3));
-                ngayTra = format.parse(c.getString(4));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            String gioNhan = c.getString(5);
-            String gioTra = c.getString(6);
-            int room_id = c.getInt(7);
-            String note = c.getString(8);
-            order x = new order(id, user_id, number, ngayNhan, ngayTra, gioNhan, gioTra, room_id, note);
-            list.add(x);
-            c.moveToNext();
-        }
-        c.close();
-        return list;
-    }
-
-    public long AddOrder(order x) {
-        ContentValues value = new ContentValues();
-        value.put("user_id", x.getUser_id());
-        value.put("number_people", x.getNumber_people());
-        value.put("booking_date", format.format(x.getBooking_date()));
-        value.put("return_date", format.format(x.getReturn_date()));
-        value.put("time_checkin", x.getTime_checkin());
-        value.put("time_checkout", x.getTime_checkout());
-        value.put("room_id", x.getRoom_id());
-        value.put("note", x.getNote());
-        return db.insert("order_tb", null, value);
-    }
-
-    public long UpdateOrder(order x) {
-        ContentValues value = new ContentValues();
-        value.put("user_id", x.getUser_id());
-        value.put("number_people", x.getNumber_people());
-        value.put("booking_date", format.format(x.getBooking_date()));
-        value.put("return_date", format.format(x.getReturn_date()));
-        value.put("time_checkin", x.getTime_checkin());
-        value.put("time_checkout", x.getTime_checkout());
-        value.put("room_id", x.getRoom_id());
-        value.put("note", x.getNote());
-        return db.update("order_tb", value, "id=?", new String[]{String.valueOf(x.getId())});
-    }
-
-    public void DeleteOrder(int ID) {
-        db.delete("order_tb", "id=?", new String[]{String.valueOf(ID)});
-    }
-
-    public List<roomImage> getRoomImg(String sql, String... args) {
-        List<roomImage> list = new ArrayList<>();
-        Cursor c = db.rawQuery(sql, args);
-        c.moveToFirst();
-        while (!c.isAfterLast()) {
-            int id = c.getInt(0);
-            int room_id = c.getInt(1);
-            int image = c.getInt(2);
-            roomImage x = new roomImage(id,room_id,image);
-            list.add(x);
-            c.moveToNext();
-        }
-        c.close();
-        return list;
-    }
-
-    public long AddRoomImg(roomImage x) {
-        ContentValues value = new ContentValues();
-        value.put("room_id", x.getRoom_id());
-        value.put("image", x.getImage());
-        return db.insert("roomImage_tb", null, value);
-    }
-
-    public void DeleteRoomImg(int ID) {
-        db.delete("roomImage_tb", "id=?", new String[]{String.valueOf(ID)});
+    public void DeleteRoom(int ID){
+        db.delete("room_tb","id=?",new String[]{String.valueOf(ID)});
     }
 
 }
