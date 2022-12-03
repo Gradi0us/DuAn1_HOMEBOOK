f8d34121537fe4168dbf541fd307a805fd5dfb2e
loinguyen10 <loinguyengamer2504@gmail.com>
2022-12-04 00:25:33 +0700

    Update FavouriteFragment.java

--- HOMEBOOK/app/src/main/java/com/example/home_book/fragment/FavouriteFragment.java
+++ HOMEBOOK/app/src/main/java/com/example/home_book/fragment/FavouriteFragment.java
@@ -2,6 +2,8 @@ package com.example.home_book.fragment;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -15,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.home_book.DAO.DAO;
 import com.example.home_book.R;
@@ -31,6 +34,8 @@ public class FavouriteFragment extends Fragment {
     TextView txtTrong;
     ArrayList<Favourite> list1;
     String user_id = "";
+    ArrayList<Room> list2 = new ArrayList<>();
+    DAO dao;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,15 +44,13 @@ public class FavouriteFragment extends Fragment {
 
         recyclerView = view.findViewById(R.id.favouriteRecyclerView);
         txtTrong = view.findViewById(R.id.txtTrong);
-        DAO dao = new DAO(getActivity());
+        dao = new DAO(getActivity());
 
         SharedPreferences sP = getActivity().getSharedPreferences("User_File", MODE_PRIVATE);
         String email = sP.getString("Email", "");
         String pass = sP.getString("Password", "");
 
         txtTrong.setText("Vui lòng đăng nhập để xem yêu thích");
-
-        ArrayList<Room> list2 = new ArrayList<>();
 
         if (dao.checkLogin(email, pass)) {
             user x = dao.get1User("select * from user_tb where email = ?", email);
@@ -72,11 +75,37 @@ public class FavouriteFragment extends Fragment {
             }
         }
 
+        LoadData();
+
+        return view;
+    }
+
+    public void showDialogXoa(int ID){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Bạn có muốn xóa không?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("CÓ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dao.DeleteFavourite(ID);
+                Toast.makeText(getActivity(), "Xóa Thành Công",Toast.LENGTH_SHORT).show();
+                LoadData();
+            }
+        });
+        builder.setNegativeButton("KO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        android.app.AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void LoadData(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         HomeBookApdater homeBookApdater = new HomeBookApdater(getContext(), list2, getActivity());
         recyclerView.setAdapter(homeBookApdater);
-
-        return view;
     }
 }
